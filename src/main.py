6dec69f8483b30631b327a02fdb6ac52910d4834
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-01-12 22:48:50 +0000

    Lean (#22)
    * add champion image
    
    * build the context more progressively
    
    Co-authored-by: Cyril Lopez <cyrlop06@hotmail.com>

--- src/main.py
+++ src/main.py
@@ -43,17 +43,23 @@ def summoner_get(
     region_code: str = "euw1",
     context: dict = get_context(),
 ):
+    # Initialize context
+    extra_context = {"request": request, "success": {}}
+    # Query summoner data
     success_summoner_data, summoner_data = riot.get_summoner_data(
         region_code, summoner, settings.api_key
     )
-    print(summoner_data)
+    # Add success status to context
+    extra_context["success"]["summoner_data"] = success_summoner_data
+    # Add summoner data to context
+    extra_context["summoner_data"] = summoner_data
     if not success_summoner_data:
+        # Add banner if summoner data query failed
         context["messages"].append(
             ("error", f"Failed to retrieve summoner data: {summoner_data}")
         )
-        success_last_games = {}
-        last_games = {}
     else:
+        # Query match history if summoner account id was successfully queried
         success_last_games, last_games = riot.get_last_games(
             region_code,
             summoner_data["accountId"],
@@ -61,24 +67,26 @@ def summoner_get(
             start_index=0,
             end_index=20,
         )
-        print(last_games)
+        # Add success status to context
+        extra_context["success"]["last_games"] = success_last_games
     # Add champion name in each game
-    champions = riot.get_champions(release=settings.latest_release)
-    for game in last_games["matches"]:
-        game["champion_name"] = riot.get_champion_name_from_id(
-            str(game["champion"]), champions
+    if success_summoner_data and not success_last_games:
+        # Add banner if the match history query failed
+        context["messages"].append(
+            ("error", f"Failed to retrieve match history data: {last_games}")
         )
-    context.update(
-        {
-            "request": request,
-            "summoner_data": summoner_data,
-            "last_games": last_games,
-            "success": {
-                "summoner_data": success_summoner_data,
-                "last_games": success_last_games,
-            },
-        }
-    )
+    if success_summoner_data and success_last_games:
+        # Fetch champion data
+        champions = riot.get_champions(release="11.1.1")
+        # Add champion name to each game, if the match history was succesfully queried
+        for game in last_games["matches"]:
+            game["champion_name"] = riot.get_champion_name_from_id(
+                str(game["champion"]), champions
+            )
+        # Add match history data to context
+        extra_context["last_games"] = last_games
+    # Update context
+    context.update(extra_context)
     return templates.TemplateResponse("summoner.html", context)
 
 
